3059169dbecd351f99fb55cb8b4a0cff386687e2
3b-fly <member@3b-fly.eu>
2019-04-20 22:59:06 +0200

    Added "bbbfly.AppIndex.AllowSelfLinks" method.

--- build/debug/app/index.js
+++ build/debug/app/index.js
@@ -36,29 +36,52 @@ bbbfly.appindex._initIndex = function(){
 };
 bbbfly.appindex._requireLeaveDialog = function(require){
   if(!Boolean.isBoolean(require)){require = true;}
+  var fnc = bbbfly.appindex._leaveDialog;
 
   if(require){
-    if(window.attachEvent){
-      window.attachEvent("onbeforeunload",bbbfly.appindex._leaveDialog);
-    }
-    else{
-      window.onbeforeunload = bbbfly.appindex._leaveDialog;
-    }
+    if(window.attachEvent){window.attachEvent("onbeforeunload",fnc);}
+    else{window.onbeforeunload = fnc;}
   }
   else{
-    if(window.detachEvent){
-      window.detachEvent("onbeforeunload",bbbfly.appindex._leaveDialog);
-    }
-    else{
-      window.onbeforeunload = null;
-    }
+    if(window.detachEvent){window.detachEvent("onbeforeunload",fnc);}
+    else{window.onbeforeunload = null;}
   }
 };
 bbbfly.appindex._leaveDialog = function(){
   return ngTxt('bbbfly_app_index_leave');
 };
+bbbfly.appindex._allowSelfLinks = function(allow){
+  if(!Boolean.isBoolean(allow)){allow = true;}
+  var fnc = bbbfly.appindex._onclick;
+
+  if(allow){
+    if(window.detachEvent){window.detachEvent("onclick",fnc);}
+    else{window.onclick = null;}
+  }
+  else{
+    if(window.attachEvent){window.attachEvent("onclick",fnc);}
+    else{window.onclick = fnc;}
+  }
+};
+bbbfly.appindex._onclick = function(event){
+  if(!event){event = window.event;}
+
+  var node = event.target || event.srcElement;
+  if (node.tagName !== 'A') {return;}
+
+  var href = node.getAttribute('href');
+  var hrefTarget = node.getAttribute('target');
+  var hrefLocation = node.getAttribute('location');
+
+  if(hrefTarget !== '_system'){hrefTarget = '_blank';}
+  else if(hrefLocation === 'null'){ hrefLocation = 'location=no';}
+
+  window.open(href,hrefTarget,hrefLocation);
+  return false;
+};
 bbbfly.AppIndex = {
   AddURLVersion: bbbfly.appindex._addURLVersion,
   InitIndex: bbbfly.appindex._initIndex,
-  RequireLeaveDialog: bbbfly.appindex._requireLeaveDialog
+  RequireLeaveDialog: bbbfly.appindex._requireLeaveDialog,
+  AllowSelfLinks: bbbfly.appindex._allowSelfLinks
 };
